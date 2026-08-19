1f05b221465ab83e7043fa26d1cd4776263c9d66
Lightning <304288295+f35a-lightning@users.noreply.github.com>
2026-08-19 11:03:40 +0700

    Update greeting.js

--- starter/greeting.js
+++ starter/greeting.js
@@ -2,5 +2,5 @@
 // You will edit this file several times during the lab and commit each change.
 // Follow exercises.md step by step.
 
-console.log("Hello, Git!");
-console.log("Lightning");
+console.log("Hello, Git!");  // greeting
+console.log("Lightning");    // name
